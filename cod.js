057249121f29e614ff91ec56dffa3a78a5c06158
Gabriel Takaoka <90276542+GabrielTakaoka@users.noreply.github.com>
2024-05-08 16:44:11 -0300

    Add files via upload

--- cod.js
+++ cod.js
@@ -22,3 +22,16 @@ function menuOnClick() {
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+document.getElementById('olho').addEventListener('mousedown', function() {
+  document.getElementById('password').type = 'text';
+});
+
+document.getElementById('olho').addEventListener('mouseup', function() {
+  document.getElementById('password').type = 'password';
+});
+
+// Para que o password não fique exposto apos mover a imagem.
+document.getElementById('olho').addEventListener('mousemove', function() {
+  document.getElementById('password').type = 'password';
+});
